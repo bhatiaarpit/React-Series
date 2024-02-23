c0f141a56b22188b148d55a6e2896de06a194e43
Arpit Bhatia <arpitbhatia903@gmail.com>
2024-02-23 15:13:16 +0530

    react router

---
+++ 07reactRouter/src/layout.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/footer'
+import { Outlet } from 'react-router-dom'
+
+function layout() {
+  return (
+    <>
+      <Header/>
+      <Outlet/>
+      <Footer/>
+    </>
+  )
+}
+export default layout
